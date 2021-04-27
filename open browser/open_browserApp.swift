acd1d44fa49f9cd2335a6ba8ab18a806043a4b0c
Salman Ahmed <salman.great@gmail.com>
2021-04-27 14:17:14 +0100

    Initial Commit

---
+++ open browser/open_browserApp.swift
@@ -0,0 +1,17 @@
+//
+//  open_browserApp.swift
+//  open browser
+//
+//  Created by Salman Ahmed on 27/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct open_browserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ open browserTests/open_browserTests.swift
@@ -0,0 +1,33 @@
+//
+//  open_browserTests.swift
+//  open browserTests
+//
+//  Created by Salman Ahmed on 27/04/2021.
+//
+
+import XCTest
+@testable import open_browser
+
+class open_browserTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
